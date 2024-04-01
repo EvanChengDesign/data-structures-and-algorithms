754988709bdf797cee6782735e9f3b5246033907
Evan Cheng <evanchengdesign@gmail.com>
2024-04-01 18:24:05 -0400

    add answer 6

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -127,8 +127,9 @@ Note: You must use reduce for this challenge. You may not use the built-in .reve
 ------------------------------------------------------------------------------------------------ */
 
 const reversedString = (str) => {
-  // Solution code here...
+  return str.split('').reduce((accumulator, currentValue) => currentValue + accumulator, '');
 };
+
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 7 - Stretch Goal
