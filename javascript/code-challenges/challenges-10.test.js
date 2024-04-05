36db9d918fa72c51e69597c5219bd8516b010ee8
Evan Cheng <evanchengdesign@gmail.com>
2024-04-05 14:50:46 -0400

    add answer 3

--- javascript/code-challenges/challenges-10.test.js
+++ javascript/code-challenges/challenges-10.test.js
@@ -53,9 +53,19 @@ For example:
 
 return: 35
 ------------------------------------------------------------------------------------------------ */
-const totalSum = (matrix) => {
-  // Solution code here...
-};
+function totalSum(matrix) {
+  let sum = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      sum += matrix[i][j];
+    }
+  }
+
+  return sum;
+}
+
+
 
 
 /* ------------------------------------------------------------------------------------------------
