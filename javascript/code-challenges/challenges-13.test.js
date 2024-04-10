b0e968554c2e230f2da997de60805dfd9dc9bcbb
Evan Cheng <evanchengdesign@gmail.com>
2024-04-10 16:35:46 -0400

    add asnwer 5 and 6

--- javascript/code-challenges/challenges-13.test.js
+++ javascript/code-challenges/challenges-13.test.js
@@ -115,9 +115,9 @@ Return an array containing all the matches.
 ------------------------------------------------------------------------------------------------ */
 
 const isCapitalized = (str) => {
-  // Solution code here...
+  const matches = str.match(/\b[A-Z][a-z]*\b/g);
+  return matches || [];
 };
-
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 6
 
@@ -125,7 +125,8 @@ Write a function named citiesAtoJ that takes in an array of city names and uses 
 ------------------------------------------------------------------------------------------------ */
 
 const citiesAtoJ = (arr) => {
-  // Solution code here...
+  const pattern = /^[A-J]/i;
+  return arr.filter(city => pattern.test(city));
 };
 
 /* ------------------------------------------------------------------------------------------------
