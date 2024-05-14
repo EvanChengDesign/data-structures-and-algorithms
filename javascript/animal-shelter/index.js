166ac4c5ff65bec73cf5db0668f71cdadc9d4d06
Evan Cheng <evanchengdesign@gmail.com>
2024-05-14 17:14:04 -0400

    add challenge 10

---
+++ javascript/animal-shelter/index.js
@@ -0,0 +1,21 @@
+class QueueUsingStacks {
+  constructor() {
+    this.stack1 = [];
+    this.stack2 = [];
+  }
+
+  enqueue(value) {
+    this.stack1.push(value);
+  }
+
+  dequeue() {
+    if (this.stack2.length === 0) {
+      while (this.stack1.length > 0) {
+        this.stack2.push(this.stack1.pop());
+      }
+    }
+    return this.stack2.length > 0 ? this.stack2.pop() : null;
+  }
+}
+
+module.exports = QueueUsingStacks;
